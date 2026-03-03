04ca488218f89111957641657701aaa4a06603a3
Cyrius <desagazancyrius@gmail.com>
2026-03-03 16:25:04 +0100

    fix: test

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,6 +1,6 @@
 from config.config import Config
 from engine.game_of_life import next_board_state
-
+from rules.rules import classic_rules
 
 def test_next_board_state_dead_cell_rule():
     """Test makes sure that DEAD cells with no live neighbors stay DEAD."""
@@ -10,9 +10,10 @@ def test_next_board_state_dead_cell_rule():
         [Config.DEAD, Config.DEAD, Config.DEAD],
     ]
 
-    result_board = next_board_state(board)
+    result_board, ever_alive = next_board_state(board, classic_rules, set())
 
     assert result_board[1][1] == Config.DEAD
+    assert not ever_alive
 
 def test_next_board_state_dead_cell_live_rule():
     """Test makes sure that DEAD cells with 3 live neighbors come ALIVE."""
@@ -22,10 +23,10 @@ def test_next_board_state_dead_cell_live_rule():
         [Config.ALIVE, Config.DEAD, Config.DEAD],
     ]
 
-    result_board = next_board_state(board)
+    result_board, ever_alive = next_board_state(board, classic_rules, {(0,0), (1,0), (2,0)})
 
     assert result_board[1][1] == Config.ALIVE
-
+    assert (1,1) in ever_alive
 
 def test_next_board_state_blinker():
     """Test to makes sure that a Blinker works."""
@@ -41,7 +42,10 @@ def test_next_board_state_blinker():
         [Config.DEAD, Config.DEAD, Config.DEAD],
     ]
 
-    assert next_board_state(blinker_board) == expected
+    return_board, ever_alive = next_board_state(blinker_board, classic_rules, {(0,1), (1,1), (2,1)})
+
+    assert return_board == expected
+    assert (2, 2) not in ever_alive
 
 def test_next_board_state_alive_cell_overpopulation():
     """Check that live cells die when they have more than 3 live neighbors"""
@@ -51,7 +55,7 @@ def test_next_board_state_alive_cell_overpopulation():
         [Config.DEAD, Config.ALIVE, Config.DEAD],
     ]
 
-    result_board = next_board_state(board)
+    result_board, ever_alive = next_board_state(board, classic_rules, {(0,1), (1,0), (1,1), (1,2), (2,1)})
 
     assert result_board[1][1] == Config.DEAD
 
@@ -63,7 +67,7 @@ def test_next_board_state_alive_cell_underpopulation():
         [Config.DEAD, Config.DEAD, Config.DEAD],
     ]
 
-    result_board = next_board_state(board)
+    result_board, ever_alive = next_board_state(board, classic_rules, {(0,2), (1,1)})
 
     assert result_board[1][1] == Config.DEAD
 
@@ -75,7 +79,7 @@ def test_next_board_state_corner_cell():
         [Config.DEAD, Config.DEAD, Config.DEAD],
     ]
 
-    result_board = next_board_state(board)
+    result_board, ever_alive = next_board_state(board, classic_rules, {(0,1), (0,2), (1,0)})
 
     assert result_board[0][0] == Config.DEAD
 
@@ -88,6 +92,6 @@ def test_next_board_state_border_cell():
         [Config.DEAD, Config.DEAD, Config.DEAD],
     ]
 
-    result_board = next_board_state(board)
+    result_board, ever_alive = next_board_state(board, classic_rules, {(1,0), (1,1), (1,2)})
 
     assert result_board[2][1] == Config.ALIVE
